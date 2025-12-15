b18e48b314f07dc38981cedab1dec3995a5b81cc
thiagosslima <thiagosslima@MacBook-Pro-M4-Thiago-Soares.local>
2025-12-15 19:41:01 -0300

    feat: first-commit

---
+++ src/main/java/com/igrejared/emprestaai/EmprestaaiApplication.java
@@ -0,0 +1,13 @@
+package com.igrejared.emprestaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestaaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestaaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igrejared/emprestaai/EmprestaaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igrejared.emprestaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestaaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
